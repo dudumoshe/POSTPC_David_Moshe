8f4c995779353a195b180b03ecbae463fe74c91e
dudumoshe <moshe.dudu@gmail.com>
2020-04-21 23:16:29 +0300

    Ex1: textView, button and EditText (David Moshe 203384540).

--- app/src/main/java/com/example/postpc_david_moshe/MainActivity.java
+++ app/src/main/java/com/example/postpc_david_moshe/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +14,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.add_item_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TextView textView = findViewById(R.id.items_list_text_view);
+                EditText editText = findViewById(R.id.insert_items_edit_text);
+                textView.setText(editText.getText());
+                editText.setText("");
+            }
+        });
     }
 }
